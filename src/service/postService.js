60ec2c8b1ca41686872e9162b593b2d5a0ff1604
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-12 21:47:44 -0300

    v.0.1.9
    Ajuste link API

--- src/service/postService.js
+++ src/service/postService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://api-not-chan-8zrapl5h8-steezus-projects.vercel.app/";
 
 export async function getPosts() {
   const response = await fetch(`${API_URL}/posts`);
